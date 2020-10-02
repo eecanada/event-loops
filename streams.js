c5144409b69f0b3ade2b528a579cc2398bb317d3
Eder Canada <canada.eder@gmail.com>
2020-10-01 19:38:44 -0700

    readab;e chunk

--- streams.js
+++ streams.js
@@ -5,14 +5,28 @@ const http = require('http')
 
 const server = http.createServer()
 
-
 server.on('request', (req,res)=>{
-  //solution 1 - will need to load entire file 
-  fs.readFile('./test-file.txt',(err, data)=>{
-    if(err) console.log(err)
-    res.end(data)
+  // //solution 1 - will need to load entire file 
+  // fs.readFile('./test-file.txt',(err, data)=>{
+  //   if(err) console.log(err)
+  //   res.end(data)
+  // })
+
+  // Solution 2 - streams, read chunk by chunk
+  const readable = fs.createReadStream('./test-filee.txt')
+  readable.on('data', (chunk)=>{
+    res.write(chunk)
+  })
+  readable.on('end', ()=>{
+    res.end()
+  })
+  readable.on('error', err =>{
+    console.log(err)
+    res.statusCode = 500
+    res.end('file not found')
   })
 }) 
+
 
 
 server.listen(8000, '127.0.0.1', ()=>{
