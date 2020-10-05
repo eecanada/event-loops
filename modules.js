7b50f377e048d6958e0f2683846ec96912d0c31d
Eder Canada <canada.eder@gmail.com>
2020-10-05 10:12:43 -0700

    c add

--- modules.js
+++ modules.js
@@ -1,5 +1,8 @@
 console.log(arguments)
 console.log(require('module').wrapper)
 
+//require my own module
+const C = require('./test-module-1')
 
-
+const calc1 = new C()
+console.log(calc1.add(2,3))
